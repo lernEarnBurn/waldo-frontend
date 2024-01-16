c0ac76d22ba2dc081ca0921742b6f5995228c589
lernEarnBurn <lerners124@gmail.com>
2024-01-16 20:41:17 +0200

    maybe need to update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 
+
 import { StartMenu } from './components/StartMenu'
 import { MainMenu } from './components/MainMenu'
 import { LevelOne } from './components/levels/LevelOne'
@@ -16,11 +17,13 @@ function App() {
     <AnimatePresence mode="wait" initial={false}>
       <Routes location={location} key={location.pathname}>
         <Route path='/waldo-frontend/' element={<StartMenu/>}/>
-        <Route path='/waldo-frontend/main-menu/' element={<MainMenu/>}/>
-        <Route path='/waldo-frontend/level-one/' element={<LevelOne/>}/>
-        <Route path='/waldo-frontend/level-two/' element={<LevelTwo/>}/>
-        <Route path='/waldo-frontend/level-three/' element={<LevelThree/>}/>
-        <Route path='/waldo-frontend/:level/leaderboard/' element={<Leaderboard/>}/>
+        <Route path='/waldo-frontend/main-menu' element={<MainMenu/>}/>
+        <Route path='/waldo-frontend/level-one' element={<LevelOne/>}/>
+        <Route path='/waldo-frontend/level-two' element={<LevelTwo/>}/>
+        <Route path='/waldo-frontend/level-three' element={<LevelThree/>}/>
+        <Route path='/waldo-frontend/:level/leaderboard' element={<Leaderboard/>}/>
+
+
       </Routes>
     </AnimatePresence>
   )
