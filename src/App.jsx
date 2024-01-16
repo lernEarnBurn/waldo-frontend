9ff8325b6955b9c967e7761326ade8ea9e9325d8
lernEarnBurn <lerners124@gmail.com>
2024-01-16 18:49:40 +0200

    tweaked the urls with react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,11 @@ function App() {
     <AnimatePresence mode="wait" initial={false}>
       <Routes location={location} key={location.pathname}>
         <Route path='/waldo-frontend/' element={<StartMenu/>}/>
-        <Route path='/waldo-frontend/main-menu' element={<MainMenu/>}/>
-        <Route path='/waldo-frontend/level-one' element={<LevelOne/>}/>
-        <Route path='/waldo-frontend/level-two' element={<LevelTwo/>}/>
-        <Route path='/waldo-frontend/level-three' element={<LevelThree/>}/>
-        <Route path='/waldo-frontend/:level/leaderboard' element={<Leaderboard/>}/>
+        <Route path='/main-menu' element={<MainMenu/>}/>
+        <Route path='/level-one' element={<LevelOne/>}/>
+        <Route path='/level-two' element={<LevelTwo/>}/>
+        <Route path='/level-three' element={<LevelThree/>}/>
+        <Route path='/:level/leaderboard' element={<Leaderboard/>}/>
 
 
       </Routes>
